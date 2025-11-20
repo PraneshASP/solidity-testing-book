42b1091b3bbb88196165069b1386cb139e9beb03
PraneshASP <praneshas2000@gmail.com>
2025-11-20 21:06:24 +0530

    feat: cleanup

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Smart Contract Testing Handbook',
+  title: 'Solidity Testing Handbook',
+  description: 'A comprehensive guide to testing Solidity smart contracts, covering testing strategies and best practices.',
+  iconUrl: './favicon.png',
+  ogImageUrl: './og.jpg',
   sidebar: [
     { text: 'Introduction', link: '/' },
     {
       text: 'Basic Testing',
       items: [
-        { text: 'Overview', link: '/basic/overview'},
+        { text: 'Overview', link: '/basic/overview' },
         { text: 'Unit tests', link: '/basic/unit-tests' },
         { text: 'Integration tests', link: '/basic/integration-tests' },
         { text: 'Fork tests', link: '/basic/fork-tests' },
@@ -22,9 +25,9 @@ export default defineConfig({
         { text: 'Differential Tests', link: '/advanced/differential-tests' },
         { text: 'Lifecycle Tests', link: '/advanced/lifecycle-tests' },
         {
-          text: 'Scenario Tests',link: '/advanced/scenario-tests',
+          text: 'Scenario Tests',
+          link: '/advanced/scenario-tests',
           items: [
-            
             { text: 'Building a scenario test runner', link: '/advanced/scenario-test-runner' },
           ],
         },
@@ -36,7 +39,8 @@ export default defineConfig({
     { text: 'Branching Tree Technique', link: '/branching-tree-technique' },
     { text: 'Swiss Cheese Method', link: '/swiss-cheese' },
   ],
-   theme: { 
-    colorScheme: 'dark'
-  },
+
+  theme: {
+    colorScheme: 'dark',
+  }
 })
